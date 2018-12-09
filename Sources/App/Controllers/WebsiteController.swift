3fc8e51bb78fc462b5b06ab5effc934357b555ad
Tommi Kivimäki <tommi.kivimaki@caravanstudio.fi>
2018-12-09 21:01:17 +0200

    createMessageHandler & createMessagePostHandler routes

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -11,12 +11,12 @@ import Leaf
 struct WebsiteController: RouteCollection {
   func boot(router: Router) throws {
     router.get(use: indexHandler)
+    router.get("messages", "create", use: createMessageHandler)
+    router.post(CreateMessageData.self, at: "messages", "create", use: createMessagePostHandler)
   }
   
   
   func indexHandler(_ req: Request) throws -> Future<View> {
-//    let context = IndexContext()
-//    return try req.view().render("index", context)
     return Message.query(on: req).all().flatMap(to: View.self) { messages in
       let messageData = messages.isEmpty ? nil : messages
       let context = IndexContext(messages: messageData)
@@ -24,19 +24,21 @@ struct WebsiteController: RouteCollection {
     }
   }
   
+  func createMessageHandler(_ req: Request) throws -> Future<View> {
+    return try req.view().render("createComment")
+  }
   
-//  func indexHandler(_ req: Request) throws -> Future<View> {
-//    // Check if request contains authenticated user
-//    let userLoggedIn = try req.isAuthenticated(User.self)
-//    return UserComment.query(on: req)
-//      .all()
-//      .flatMap(to: View.self) { userComments in
-//        let userCommentsData = userComments.isEmpty ? nil : userComments
-//        // Check if cookies-accepted exists
-//        let showCookieMessage = req.http.cookies["cookies-accepted"] == nil
-//        let context = IndexContext(title: "Comment Box", userLoggedIn: userLoggedIn, userComments: userCommentsData, showCookieMessage: showCookieMessage)
-//        return try req.view().render("index", context)
-//    }
-//  }
+  func createMessagePostHandler(_ req: Request, data: CreateMessageData) throws -> Future<Response> {
+    let message = Message(message: data.message)
+    let redirect = req.redirect(to: "/")
+    return message.save(on: req).transform(to: redirect)
+  }
 
+}
+
+
+// Decode createMessagePostHandler request body to this
+struct CreateMessageData: Content {
+  let message: String
+  let csrfToken: String?
 }
