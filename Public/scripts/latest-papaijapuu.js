f29dcaccc87b8d44bcba6382e1a9d4fb44957157
Tommi Kivimäki <tommi@tommikivimaki.com>
2018-12-20 20:56:13 +0200

    clientHeight

--- Public/scripts/latest-papaijapuu.js
+++ Public/scripts/latest-papaijapuu.js
@@ -1,17 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
-  sendHeight()
-})
+                          sendHeight()
+                          })
 
 window.addEventListener("resize", function() {
-	sendHeight()
-})
+                        sendHeight()
+                        })
 
 function sendHeight() {
-  let docHeight = Math.max(
-    document.body.scrollHeight, document.documentElement.scrollHeight,
-    document.body.offsetHeight, document.documentElement.offsetHeight,
-    document.body.clientHeight, document.documentElement.clientHeight
-  )
-  let message = {height: docHeight}
-  parent.postMessage(message,"*")
+  let indexMessageList = document.getElementById("indexMessageList")
+  
+  if (indexMessageList !== null && indexMessageList !== undefined) {
+    let elementHeight = indexMessageList.clientHeight
+    let message = {height: elementHeight}
+    parent.postMessage(message,"*")
+  }
+  //  let docHeight = Math.max(
+  //    document.body.scrollHeight, document.documentElement.scrollHeight,
+  //    document.body.offsetHeight, document.documentElement.offsetHeight,
+  //    document.body.clientHeight, document.documentElement.clientHeight
+  //  )
+  //  let docHeight = document.documentElement.clientHeight
+  //  let message = {height: docHeight}
+  //  parent.postMessage(message,"*")
 }
