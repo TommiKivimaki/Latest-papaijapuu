7a8552a6bbbf5c8059572d212198853975318ed4
Tommi Kivimäki <tommi.kivimaki@caravanstudio.fi>
2018-12-27 20:13:43 +0200

    status: loaded message

--- Public/scripts/latest-papaijapuu.js
+++ Public/scripts/latest-papaijapuu.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
+                          sendLoaded()
                           sendHeight()
                           })
 
@@ -6,12 +7,17 @@ window.addEventListener("resize", function() {
                         sendHeight()
                         })
 
+function sendLoaded() {
+  let message = {status: "loaded"}
+  parent.postMessage(message,"*")
+}
+
 function sendHeight() {
   let indexMessageList = document.getElementById("indexMessageList")
   
   if (indexMessageList !== null && indexMessageList !== undefined) {
-//    let bodyHeight = document.body.scrollHeight
-    let bodyHeight = $(document).height()
+    let bodyHeight = document.body.scrollHeight
+//    let bodyHeight = $(document).height()
     let message = {height: bodyHeight}
     parent.postMessage(message,"*")
   }
