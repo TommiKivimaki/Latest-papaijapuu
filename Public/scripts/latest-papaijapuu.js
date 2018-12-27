2922f6dc4802513a119de462a05e9adb3e465614
Tommi Kivimäki <tommi.kivimaki@caravanstudio.fi>
2018-12-27 23:48:44 +0200

    Height of indexContentArea

--- Public/scripts/latest-papaijapuu.js
+++ Public/scripts/latest-papaijapuu.js
@@ -17,7 +17,8 @@ function sendHeight() {
   
   if (indexMessageList !== null && indexMessageList !== undefined) {
 //    let bodyHeight = document.body.scrollHeight
-    let bodyHeight = $(document).height()
+//    let bodyHeight = $(document).height()
+    let bodyHeight = $("indexContentArea").height()
     let message = {height: bodyHeight}
     parent.postMessage(message,"*")
   }
